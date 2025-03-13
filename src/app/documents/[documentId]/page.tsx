8ff3bc0322a46f3c8b3bb69643673e9ee645d487
Oshan Niroshana <oshan@nexcodian.com>
2025-03-13 19:40:39 +0530

    update

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,12 +1,6 @@
 import Editor from './editor'
 
-interface DocumentIdPageProps {
-  params: Promise<{ documentId: string }>
-}
-
-const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
-  const { documentId } = await params
-
+const DocumentIdPage = async () => {
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
       <Editor />
